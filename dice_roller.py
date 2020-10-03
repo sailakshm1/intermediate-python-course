661e08e4664540879c0689d03314bded8cb77208
Sai Lakshmi Macharla <macharlasailakshmi@gmail.com>
2020-10-03 19:08:06 +0530

    git commit -m "Now rolling two dice"

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,11 @@
 import random
 def main():
-    roll=random.randint(1,6)
-    print(f'You rolled a {roll}')
-
+    dice_rolls=2
+    roll_sum=0
+    for i in range(0,dice_rolls):
+        roll=random.randint(1,6)
+        roll_sum += roll
+        print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {roll_sum}')
 if __name__== "__main__":
   main()
